63868d460b7aeb8a0154256837210b3c10b01fb3
Welber33 <jwwork21@gmail.com>
2022-03-21 09:30:05 -0300

    Finalizando inserção

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -27,7 +27,7 @@ interface TransactionProviderProps {
 
 interface TransactionsContextData {
     transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => void;
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -43,8 +43,18 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
             .then(response => settransactions(response.data.transactions))
     }, []);
 
-    function createTransaction(transaction: TransactionInput) {
-        api.post('/transactions', transaction)
+    async function createTransaction(transactionInput: TransactionInput) {
+        const response = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt: new Date(),
+        })
+
+        const { transaction } = response.data;
+
+        settransactions([
+            ...transactions,
+            transaction,
+        ]);
     }
 
     return (
